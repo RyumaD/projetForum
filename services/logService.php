0e67323e9d760295695e6a937a26461e7fa6da78
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-09 11:54:49 +0200

    Commentaires dans logService.php et regService.php

--- services/logService.php
+++ services/logService.php
@@ -8,6 +8,16 @@ $usr = verifusername($username);
 $mail = verifemail($email);
 $pass = verifpassword($password);
 
+/*
+ * Alfonso: Tu as compris très vite le fonctionnement de notre petit
+ * système MVC. Tu as bien exécuté l'application des fonction dans le
+ * fichier destiné à cette effet. càd le model/fonctions.php Bravo
+ * Par contre je ne sais pas si pour se connecter il faille le username
+ * l'email ET le password. Normalement on a que deux champs.
+ * Je vais te compliquer la tache en te disant qu'il faut l'un ou l'autre
+ * et pas les deux. Je pense que tu peux faire ça.
+ * */
+
 if(isset($username) AND isset($password) AND isset($email)){
     if(count($usr)>= 1 AND count($mail)>= 1 AND count($pass)>= 1){
         header("location: ../index.php?page=principal&controle=success");
--- services/regService.php
+++ services/regService.php
@@ -15,6 +15,14 @@ if(isset($username) AND isset($password) AND isset($confpword) AND isset($email)
                 $erreur = true;
         }
         else{
+            /*
+             * Bravo encore tu commences très bien avec le checking des inputs user
+             * avec un regex mais je pense que ce petit paquet de code tu peux en faire une fonction
+             * tu es susceptible de vérifier des emails plusieurs fois dans un forum
+             * Ça serait bien d'en faire une fonction.
+             * L'appel en base de données doit être une fonction aussi..
+             * mais je suppose que tu n'as pas encore eu le temps de faire.
+             * */
                 if(strlen($password) > 8 AND $password == $confpword AND strlen($username) > 4){
                         if(preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $email)){
                                 $connexion = getconnexion();
@@ -36,6 +44,7 @@ else{
         $erreur = true; 
 }
 if($erreur == true){
+    /* Alfonso: essaie de faire un die après chaque header(); */
     header("location: ../index.php?page=login&controle=failed");    
 }
 else{
